ebb1c8901589b0733d240e7b9b10d517e5203f8a
YxY <13453892580@163.com>
2024-08-05 01:28:16 +0800

    提交

--- src/components/Links.vue
+++ src/components/Links.vue
@@ -43,7 +43,7 @@
 <script setup>
 import { Icon } from "@vicons/utils";
 // 可前往 https://www.xicons.org 自行挑选并在此处引入
-import { Link, Blog, CompactDisc, Cloud, Compass, Book, Fire, LaptopCode, AccountBookFilled } from "@vicons/fa"; // 注意使用正确的类别
+import { Link, Blog, CompactDisc, Cloud, Compass, Book, Fire, LaptopCode, Microblog } from "@vicons/fa"; // 注意使用正确的类别
 import { mainStore } from "@/store";
 import { Swiper, SwiperSlide } from "swiper/vue";
 import { Pagination, Mousewheel } from "swiper/modules";
@@ -70,7 +70,7 @@ const siteIcon = {
   Book,
   Fire,
   LaptopCode,
-  AccountBookFilled
+  Microblog
 };
 
 // 链接跳转
